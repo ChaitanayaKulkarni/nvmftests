d26192b0804580021c0c958de96860cf7b2762d0
Chaitanya Kulkarni <ckulkarnilinux@gmail.com>
2017-07-29 02:08:15 -0700

    nvmftests : add documentation for target_config_generator

--- target_config_generator.py
+++ target_config_generator.py
@@ -22,15 +22,30 @@
 import json
 
 class port:
+    """
+    Represents target port config generator.
+
+        - Attributes:
+            - port_id: unique port identification number.
+            - port : list of the ports.
+            - port_dict : dictionary to hold port attributes.
+            - referrals : list of target port referals.
+            - subsystems : list of the subsystems associated with this port.
+    """
     def __init__(self, port_id):
         self.port_id = port_id
-        self.port = []
         self.port_dict = {}
         self.addr = {}
         self.referrals = [None]
         self.subsystems = []
 
     def build_addr(self):
+        """ build address attributes for this port.
+            - Args :
+                  - None
+            -Returns :
+                  - None
+        """
         self.addr['adrfam'] = ""
         self.addr['traddr'] = ""
         self.addr['treq'] = "not specified"
@@ -39,10 +54,22 @@ class port:
         self.port_dict['addr']  = self.addr
 
     def build_subsystems(self, subsys_list):
+        """ Build subsystem list associated with this port.
+            - Args :
+                  - None
+            -Returns :
+                  - True on success, False on failure.
+        """
         self.subsystem = subsys_list
         self.port_dict['subsystems']  = self.subsystem
 
     def build_port(self, nqn_list):
+        """ Builds port addr, subsystems, id, and referrals.
+            - Args :
+                  - List of the subsystem nqn associated with this port.
+            -Returns :
+                  - Port dictionary with valid values.
+        """
         self.build_addr()
         self.build_subsystems(nqn_list)
         self.port_dict['portid'] = self.port_id
@@ -100,6 +127,18 @@ class subsystem:
         return ss_entry
 
 class target_config:
+    """
+    Represents target config generator.
+
+        - Attributes:
+            - ss_list : list of subsystems associsted with this target.
+            - port_list : list of the ports associated with this target.
+            - config_file_path : path name for generated config file.
+            - nr_subsys : number of subsystems present in this target.
+            - nr_ns : number of namespaces associated with each subsystems.
+            - nr_loop_dev : number of loop devices to be used for all
+                            the namespaces.
+    """
 
     def __init__(self, config_file_path, nr_subsys, nr_ns, nr_loop_dev):
 
@@ -112,14 +151,26 @@ class target_config:
 
     def pp_json(self, json_thing, sort=True, indents=4):
 
+        """ Prints formatted JSON output.
+            - Args :
+                  - json_thing : JSON string.
+                  - sort : flag to sort JSON values before formatting.
+                  - idents : identation width.
+            -Returns :
+                  - formatted JSON string.
+        """
         if type(json_thing) is str:
             return json.dumps(json.loads(json_thing),
             sort_keys=sort, indent=indents)
-        else:
-            return json.dumps(json_thing, sort_keys=sort, indent=indents)
+        return json.dumps(json_thing, sort_keys=sort, indent=indents)
 
     def build_target_subsys(self):
-
+        """ Builds the Target config and dumps in JSON format.
+            - Args :
+                  - None
+            -Returns :
+                  - None.
+        """
         nqn_list = []
         ss_list = []
         port_list = []
