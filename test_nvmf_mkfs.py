754123b44459786cd22afc25aaf99585addaa11a
Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>
2017-08-03 13:09:50 -0700

    nvmftests : move common code to base class

--- test_nvmf_mkfs.py
+++ test_nvmf_mkfs.py
@@ -40,10 +40,6 @@ class TestNVMFMKFS(NVMeOFTest):
 
     def __init__(self):
         NVMeOFTest.__init__(self)
-        self.loopdev = None
-        self.host_subsys = None
-        self.target_subsys = None
-
         self.setup_log_dir(self.__class__.__name__)
         self.loopdev = Loopback(self.mount_path, self.data_size,
                                 self.block_size, self.nr_loop_dev)
@@ -51,19 +47,12 @@ class TestNVMFMKFS(NVMeOFTest):
     def setUp(self):
         """ Pre section of testcase """
         self.loopdev.init()
-        target_type = "loop"
-        self.target_subsys = NVMeOFTarget(target_type)
-        ret = self.target_subsys.config(self.target_config_file)
-        assert_equal(ret, True, "ERROR : target config failed")
-        self.host_subsys = NVMeOFHost(target_type)
-        ret = self.host_subsys.config(self.target_config_file)
-        assert_equal(ret, True, "ERROR : host config failed")
+        super(TestNVMFMKFS, self).common_setup()
 
     def tearDown(self):
         """ Post section of testcase """
-        self.host_subsys.delete()
-        self.target_subsys.delete()
         self.loopdev.delete()
+        super(TestNVMFMKFS, self).common_tear_down()
 
     def test_mkfs(self):
         """ Testcase main """
