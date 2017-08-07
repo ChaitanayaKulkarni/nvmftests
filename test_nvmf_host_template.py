59758f4ce0aedcb4587db644da7ff4c8c288ab16
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:28:01 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_host_template.py
+++ test_nvmf_host_template.py
@@ -27,9 +27,10 @@ NVMF host template :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFHostTemplate(NVMFTest):
