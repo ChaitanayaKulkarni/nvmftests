968510e5be6e0516b1368b7fafdf4f6118d7d3ce
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:30:04 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_scan_target.py
+++ test_nvmf_scan_target.py
@@ -27,9 +27,10 @@ NVMF scan target :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFScanTarget(NVMFTest):
