4f6bc5b5472740655d088449188ff4bcfb67e16e
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:28:27 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_id_ns.py
+++ test_nvmf_id_ns.py
@@ -27,9 +27,10 @@ NVMF test id-ns on each namespace :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFIdentifyNamespace(NVMFTest):
