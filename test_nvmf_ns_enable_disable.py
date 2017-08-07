aa04a421cefb0a1926c8dc84a0d9a62f436a63c3
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:28:50 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_ns_enable_disable.py
+++ test_nvmf_ns_enable_disable.py
@@ -27,9 +27,10 @@ NVMF test enable disable target namespace :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFNSEnableDisable(NVMFTest):
