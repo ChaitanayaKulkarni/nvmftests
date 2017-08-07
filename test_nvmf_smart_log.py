3da870a81b5556a703e4b3a2f5266571d5c5cfa9
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:30:23 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_smart_log.py
+++ test_nvmf_smart_log.py
@@ -27,9 +27,10 @@ NVMF test smart log on all controllers :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFSmartLog(NVMFTest):
