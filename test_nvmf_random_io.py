d5ab03b43614a26901479a173f0b42173f903dec
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:29:29 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_random_io.py
+++ test_nvmf_random_io.py
@@ -27,9 +27,10 @@ NVMF randomly select a controller and run IOs :-
     5. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFRandomFabric(NVMFTest):
