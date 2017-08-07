3e08649e4cc646bd4cd378a1bfb3e06110008a29
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-07 15:30:13 -0700

    nvmftests : pylint warnings for package imports

--- test_nvmf_simple_io.py
+++ test_nvmf_simple_io.py
@@ -27,9 +27,10 @@ NVMF sequntially select a controller and run IOs:-
     4. Delete Target.
 """
 
+
+from nose.tools import assert_equal
 from loopback import Loopback
 from nvmf_test import NVMFTest
-from nose.tools import assert_equal
 
 
 class TestNVMFSeqIO(NVMFTest):
