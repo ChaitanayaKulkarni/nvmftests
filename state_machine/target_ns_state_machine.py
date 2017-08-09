866a3143852f2cf0ea070e2b5b368201e8d71116
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-09 13:12:37 -0700

    nvmftests : tirm down statem machine for target ns

--- state_machine/target_ns_state_machine.py
+++ state_machine/target_ns_state_machine.py
@@ -3,7 +3,8 @@ import sys
 from transitions.extensions import GraphMachine
 
 
-class TargetNSStateMachine(object):
+class TargetSubsysStateMachine(object):
+    """ Target Subsystem State Machine """
     def __init__(self, start_state):
         print (sys._getframe().f_code.co_name)
         self.start_state_dict = {}
@@ -19,25 +20,13 @@ class TargetNSStateMachine(object):
     def entry_start(self):
         print (sys._getframe().f_code.co_name)
 
-    def exit_start(self):
-        print (sys._getframe().f_code.co_name)
-
     def entry_init(self):
-        print (sys._getframe().f_code.co_name)
-
-    def exit_init(self):
         print (sys._getframe().f_code.co_name)
 
     def entry_running(self):
         print (sys._getframe().f_code.co_name)
 
-    def exit_running(self):
-        print (sys._getframe().f_code.co_name)
-
     def entry_dead(self):
-        print (sys._getframe().f_code.co_name)
-
-    def exit_dead(self):
         print (sys._getframe().f_code.co_name)
 
     def is_valid_start(self):
@@ -65,29 +54,11 @@ class TargetNSStateMachine(object):
         self.get_graph(**kwargs).draw('state.png', prog='dot')
 
 
-states = ['start', 'init', 'run', 'enable', 'disable', 'dead']
+STATES = ['start', 'init', 'run', 'enable', 'disable', 'dead']
 
-to_dead = ['enable', 'disable', 'run']
-
-transitions = [
-    {'trigger': 'start', 'source': 'start', 'dest': 'init', 'conditions':
-     'is_valid_start'},
-    {'trigger': 'init', 'source': 'init', 'dest': 'run', 'conditions':
-     'is_valid_init'},
-    {'trigger': 'run-enable', 'source': 'run', 'dest': 'enable', 'conditions':
-     'is_valid_enable'},
-    {'trigger': 'disable-enable', 'source': 'disable', 'dest': 'enable', 'conditions':
-     'is_valid_enable'},
-    {'trigger': 'run-disable', 'source': 'run', 'dest': 'disable', 'conditions':
-     'is_valid_disable'},
-    {'trigger': 'enable-disable', 'source': 'enable', 'dest': 'disable', 'conditions':
-     'is_valid_disable'},
-]
-
-
-target_ns = TargetNSStateMachine('start')
+target_ns = TargetSubsysStateMachine('start')
 machine = GraphMachine(model=target_ns,
-                       states=states,
+                       states=STATES,
                        transitions=None,
                        initial='start',
                        title="Target Namespace State Transition Diagram",
@@ -98,6 +69,6 @@ machine.add_transition('start', 'start', 'init')
 machine.add_transition('init', 'init', 'run')
 machine.add_transition('run-enable', 'run', 'enable')
 machine.add_transition('disable-enable', 'disable', 'enable')
-machine.add_transition('run-disable', 'run', 'disable') 
+machine.add_transition('run-disable', 'run', 'disable')
 machine.add_transition('enable-disable', 'enable', 'disable')
 target_ns.show_graph()
