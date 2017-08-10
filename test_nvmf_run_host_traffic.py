a2e41ff2cb9f34127a914d0dbd6f4393cbacd17f
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-10 15:12:43 -0700

    nvmftests : start using null_blk instead of loopback

--- test_nvmf_run_host_traffic.py
+++ test_nvmf_run_host_traffic.py
@@ -57,11 +57,11 @@ class TestNVMFHostTraffic(NVMFTest):
     def __init__(self):
         NVMFTest.__init__(self)
         self.setup_log_dir(self.__class__.__name__)
-        self.loopdev = Loopback(self.mount_path, self.data_size,
-                                self.block_size, self.nr_dev)
 
     def setUp(self):
         """ Pre section of testcase """
+        self.loopdev = Loopback(self.mount_path, self.data_size,
+                                self.block_size, self.nr_dev)
         self.loopdev.init()
         self.build_target_config(self.loopdev.dev_list)
         self.dd_read_traffic = {"IO_TYPE": "dd",
