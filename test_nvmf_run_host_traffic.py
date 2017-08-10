d48e529587826d62f77ef74fa83fdedf338aad92
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-10 01:35:45 -0700

    nvmftests : use device list for loopback

--- test_nvmf_run_host_traffic.py
+++ test_nvmf_run_host_traffic.py
@@ -58,8 +58,12 @@ class TestNVMFHostTraffic(NVMFTest):
         NVMFTest.__init__(self)
         self.setup_log_dir(self.__class__.__name__)
         self.loopdev = Loopback(self.mount_path, self.data_size,
-                                self.block_size, self.nr_loop_dev)
+                                self.block_size, self.nr_dev)
 
+    def setUp(self):
+        """ Pre section of testcase """
+        self.loopdev.init()
+        self.build_target_config(self.loopdev.dev_list)
         self.dd_read_traffic = {"IO_TYPE": "dd",
                                 "IODIR": "read",
                                 "THREAD": __run_traffic__,
@@ -68,16 +72,12 @@ class TestNVMFHostTraffic(NVMFTest):
                                 "BS": "4K",
                                 "COUNT": str(self.data_size / self.block_size),
                                 "RC": 0}
-
-    def setUp(self):
-        """ Pre section of testcase """
-        self.loopdev.init()
         super(TestNVMFHostTraffic, self).common_setup()
 
     def tearDown(self):
         """ Post section of testcase """
-        self.loopdev.delete()
         super(TestNVMFHostTraffic, self).common_tear_down()
+        self.loopdev.delete()
 
     def disable_target_ns(self):
         ret = True
