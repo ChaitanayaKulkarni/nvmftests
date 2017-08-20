796cd9a83a980c25c79be8fe51f8f89a7bb0be15
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2017-08-19 23:07:07 -0700

    nvmftests : add support for centralize logging helper

---
+++ utils/log/log.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2016-2017 Western Digital Corporation or its affiliates.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+#
+#   Author: Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>
+#
+""" Represents Shell Command execution.
+"""
+import json
+import logging
+
+
+class Log(object):
+
+    """
+    Represents a shell command execution.
+        - Attributes :
+    """
+
+    @staticmethod
+    def get_logger(name, element):
+        """ Returns the log level from config file.
+            - Args :
+                - element : NVMe Over Fabric subsystem element.
+            - Returns :
+                - Valid Log level on success, None otherwise.
+        """
+        logger = None
+        with open('config/nvmftests.json') as cfg_file:
+            cfg = json.load(cfg_file)
+            logger = logging.getLogger(name)
+            log_format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+            log_format += '%(filename)20s %(funcName)20s %(lineno)4d'
+            log_format += '%(pathname)s'
+            if cfg['log'][element] == "DEBUG":
+                logger.setLevel(logging.DEBUG)
+            elif cfg['log'][element] == "INFO":
+                logger.setLevel(logging.INFO)
+            elif cfg['log'][element] == "WARNING":
+                logger.setLevel(logging.WARNING)
+            elif cfg['log'][element] == "CRITICAL":
+                logger.setLevel(logging.CRITICAL)
+            elif cfg['log'][element] == "ERROR":
+                logger.setLevel(logging.ERROR)
+        return logger
