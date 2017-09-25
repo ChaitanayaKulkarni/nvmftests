2f0a51fb29b748f42a33221013a46c321a5bcea7
root <root@kdev.hgst.com>
2017-09-25 15:29:59 -0700

    nvmftests : remove shell scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `ls *.py`; 
-	do 
-		echo "_____________________Running ${i}_____________________"; 
-		TESTCASE_NAME=`echo ${i} | cut -f 1 -d '.'`; 
-		nose2 --verbose ${TESTCASE_NAME}
-done
